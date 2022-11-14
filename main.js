378f1980278122715dfda740e4d728639debf725
TheDominooos <55160640+TheDominooos@users.noreply.github.com>
2022-11-14 11:33:29 +0100

    added button functionality

--- main.js
+++ main.js
@@ -1,49 +1,33 @@
 var display=""
-
-
-
-
-
-
-
-
-function updateDisplay(){
-    document.getElementById('display').innerHTML=display;
-}
-/*
-var operationType=0;
-var number1=""; 
-var number2="";
-var display="";
 var isFirstInput=true;
-var result;
 
 
-function numberButton(input){
-    display+=input; updateDisplay()
+function numberButton(number){
+    display+=number; updateDisplay();
+}
+
+function operationButton(operation){
     if(isFirstInput){
-        number1+=input; alert("number1:"+number1);
+        display+=operation; updateDisplay();
+        isFirstInput=false;
     }else{
-        number2+=input; alert("number2:"+number2);
-        
+        calculateResult(); display+=operation; updateDisplay();
     }
+    
+}
+function calculateResult(){
+    calculation=display;
+    result=eval(calculation);
+    display=result; updateDisplay();
+    resetCalculator(); display=result;
 }
 
-function operationButton(operationType){
-    switch(operationType){
-        case 1:{
-            if (isFirstInput){
-                display+="+"; updateDisplay();
-                isFirstInput=false;
-            }else{
-                number1=Number(number1)+Number(number2);
-                display=number1+"+";
-                number2="";
-            }
-        }
-    }
+
+function resetCalculator(){
+    display="";
+    isFirstInput=true;
 }
+
 function updateDisplay(){
     document.getElementById('display').innerHTML=display;
 }
-*/
